bf9142ff714cf65c04238cd4b084d4c1efe9339e
Ruan Ferreira <droderuan@hotmail.com>
2020-06-16 15:33:32 -0300

    Remove dist folder from track

--- dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: 'a1087c6217d34719996f9385fef6d4be',
-        expiresIn: '2d',
-    },
-};
